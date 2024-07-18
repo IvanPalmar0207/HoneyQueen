f8d28a1971e784ce42ef542db9449e5edf972152
IvanPalmar07 <palmar.ivan0205@gmail.com>
2024-07-18 16:50:20 -0500

    Version 10.0 - MVP

--- Users/urls.py
+++ Users/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
 #Views
-from Users.views import registerUser, loginUser, exit, manageUsers
+from Users.views import registerUser, loginUser, exit
+#Admin Views
+#Users Management
+from Users.views import manageUsers, renderInsertUser, insertUsers, renderUpdateUser, updateUsers, deleteUsers
+#Products Management
+from Users.views import manageProducts, renderInsertProducts, deleteProducts
 
 urlpatterns = [
     path('registerUser/', registerUser, name = 'registerUser'),
     path('loginUsers/', loginUser, name = 'loginUsers'),
     path('logout/', exit, name = 'exit'),
-    path('manageUsers/', manageUsers, name = 'manageUsers')
+    #Manage Users
+    path('manageUsers/', manageUsers, name = 'manageUsers'),
+    path('renderInsertUser/', renderInsertUser, name = 'renderInsertUser'),
+    path('insertUsers/', insertUsers, name = 'insertUsers'),
+    path('renderUpdateUser/<documentNumber>', renderUpdateUser, name = 'renderUpdateUser'),
+    path('updateUsers/<documentNumber>', updateUsers, name = 'updateUsers'),
+    path('deleteUsers/<documentNumber>', deleteUsers, name = 'deleteUsers'),
+    #Manage Products
+    path('manageProducts/', manageProducts, name = 'manageProducts'),
+    path('renderInsertProducts/', renderInsertProducts, name = 'renderInsertProducts'),
+    path('deleteProducts/<productId>', deleteProducts, name = 'deleteProducts')
 ]
--- Users/views.py
+++ Users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect, render
 #User Model
 from Users.models import User
+#Product Model
+from Products.models import tb_products, tb_categories, tb_sizes, tb_referencies
 #Authenticate
 from django.contrib.auth import authenticate, login, logout
 #Login Function
@@ -54,7 +56,6 @@ def exit(request):
     messages.success(request,'Has cerrado sesión correctamente')
     return redirect('/')
 
-
 #Admin Views
 #Manage Users
 @login_required
@@ -73,30 +74,164 @@ def manageUsers(request):
         return redirect('home')
     
 @login_required
+def renderInsertUser(request):
+    if request.user.is_superuser:
+        return render(request, 'admin/newUser.html')
+    else: 
+        messages.error(request,'No tienes los permisos correspondientes')
+        return redirect('home')
+
+@login_required
 def insertUsers(request):
-    pass
+    if request.user.is_superuser:
+        if request.method == 'POST':
+            documentNumber = request.POST['documentNumber']
+            namesUser = request.POST['namesUser']
+            emailUser = request.POST['emailUser']
+            passwordUser = request.POST['passwordUser']
+
+            user = User.objects.create(documentNumber = documentNumber, fullName = namesUser, email = emailUser)
+
+            if user:
+                user.set_password(passwordUser)
+                user.save()
+                messages.success(request,'El nuevo usuario ha sido registrado correctamente, gracias.')
+                return redirect('manageUsers')
+            
+            else:
+                messages.error(request,'No se ha podido registrar al usuario correctamente, intenta nuevamente.')
+                return redirect('manageUsers')
+    else: 
+        messages.error(request,'No tienes los permisos correspondientes')
+        return redirect('home')
 
 @login_required
-def updateUsers(request):
-    pass
+def renderUpdateUser(request, documentNumber):
+    if request.user.is_superuser:
+
+        user = User.objects.get(documentNumber = documentNumber)
+
+        context = {
+            'user' : user
+        }
+
+        return render(request,'admin/updateUsers.html', context)
+    else:
+        messages.error(request,'No tienes los permisos correspondientes')
+        return redirect('home')        
 
 @login_required
-def deleteUsers(request):
-    pass
+def updateUsers(request, documentNumber):
+    if request.user.is_superuser:
+        if request.method == 'POST':
+            namesUser = request.POST['namesUser']
+            emailUser = request.POST['emailUser']
+            passwordUser = request.POST['passwordUser']
+
+            user = User.objects.get(documentNumber = documentNumber)
+
+            if user:
+                user.fullName = namesUser
+                user.email = emailUser
+                user.set_password(passwordUser)
+
+                user.save()
+
+                messages.success(request,'El usuario ha sido actualizado correctamente, gracias.')
+                return redirect('manageUsers')
+
+            else:
+                messages.error(request,'No se ha podido actualizar al usuario correctamente, intenta nuevamente.')
+                return redirect('manageUsers')           
+
+    else:
+        messages.error(request, 'No tienes los permisos correspondientes')
+        return redirect('home')
+
+@login_required
+def deleteUsers(request, documentNumber):
+    if request.user.is_superuser:
+        user = User.objects.get(documentNumber = documentNumber)
+
+        if user:
+
+            user.delete()      
+            messages.success(request,'El ususario ha sido eliminado correctamente, gracias y buen dia.')
+            return redirect('home')
+        
+        else:
+            messages.error(request,'No se ha podido eliminar al usuario correctamente, intenta nuevamente.')
+            return redirect('manageUsers')
+
+    else:
+        messages.error(request, 'No tienes los permisos correspondientes')
+        return redirect('home')
+
 
 #Manage Product
 @login_required
 def manageProducts(request):
-    pass
+    if request.user.is_superuser:
+
+        products = tb_products.objects.all()
+
+        context = {
+            'products' : products
+        }
+
+        return render(request,'admin/products/manageProducts.html', context)
+
+    else:
+        messages.error(request,'No tienes los permisos correspondientes')
+        return redirect('home')
+
+@login_required
+def renderInsertProducts(request):
+    if request.user.is_superuser:
+
+        categories = tb_categories.objects.all()
+        sizes = tb_sizes.objects.all()
+        referencies = tb_referencies.objects.all()
+
+        context = {
+            'categories' : categories,
+            'sizes' : sizes,
+            'referencies' : referencies
+        }
+
+        return render(request,'admin/products/newProducts.html', context)
+    else:
+        messages.error(request,'No tienes los permisos correspondientes')
+        return redirect('home')
 
 @login_required
 def insertProducts(request):
     pass
 
 @login_required
-def updateUsers(request):
+def renderUpdateProducts(request, productId):
     pass
 
 @login_required
-def deleteProducts(request):
+def updateProducts(request, productId):
     pass
+
+@login_required
+def deleteProducts(request, productId):
+    if request.user.is_superuser:        
+        
+        product = tb_products.objects.get(productId = productId)
+
+        if product:
+
+            product.delete()
+            messages.success(request,'El producto ha sido eliminado correctamente, gracias y buen dia.')
+            return redirect('home')
+            
+        else:
+            messages.error(request,'No se ha podido eliminar al producto correctamente, intenta nuevamente.')
+            return redirect('manageUsers')
+
+    else:
+        messages.error(request,'No tienes los permisos correspondientes')
+        return redirect('home')
